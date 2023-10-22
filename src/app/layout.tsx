87f69b7b37823af544c8298947c1d542d29ddf24
kapil619 <kapilbadokar321@gmail.com>
2023-10-22 20:57:12 +0530

    Navbar sylling with framer motion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative `}>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] `}
+      >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] -z-10 "></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left[-5rem] "></div>
-
+        <Header />
         {children}
       </body>
     </html>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import corpcommentImg from "/public/corpcomment.png";
+import rmtdevImg from "/public/rmtdev.png";
+import wordanalyticsImg from "/public/wordanalytics.png";
 
 export const links = [
     {
