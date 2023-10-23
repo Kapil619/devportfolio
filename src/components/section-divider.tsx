981374952c96dc8f9788e3227c50a227c29fbec4
kapil619 <kapilbadokar321@gmail.com>
2023-10-23 10:17:09 +0530

    Added Section Divider component

---
+++ src/components/section-divider.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+
+const SectionDivider = () => {
+  return (
+    <motion.div
+      className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ delay: 0.125 }}
+    ></motion.div>
+  );
+};
+
+export default SectionDivider;
