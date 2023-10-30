7e34cc827c13b90e94228e39ec84742acb6e71be
kapil619 <kapilbadokar321@gmail.com>
2023-10-30 19:24:49 +0530

    Created a blank projects component

---
+++ src/components/projects.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SectionHeading from "./sectionHeading";
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeading text="My Projects" />
+    </section>
+  );
+}
