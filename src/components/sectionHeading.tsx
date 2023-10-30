e6a64ceeec69bcd9681a94c691e1b65afed218fc
kapil619 <kapilbadokar321@gmail.com>
2023-10-30 19:59:59 +0530

    finished project section styling

--- src/components/sectionHeading.tsx
+++ src/components/sectionHeading.tsx
@@ -5,7 +5,9 @@ interface SectionHeadingProps {
 }
 
 const SectionHeading = ({ text }: SectionHeadingProps) => {
-  return <h2 className="text-3xl font-medium capitalize mb-8">{text}</h2>;
+  return (
+    <h2 className="text-3xl font-medium capitalize mb-8 text-center">{text}</h2>
+  );
 };
 
 export default SectionHeading;
