be14cd64f87c511cd583ba921b991131ddf20a9f
kapil619 <kapilbadokar321@gmail.com>
2023-11-09 00:14:57 +0530

    Completed Skills section

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading text="My Projects" />
       <div>
         {projectsData.map((project, index) => (
---
+++ src/components/skills.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import SectionHeading from "./sectionHeading";
+import { skillsData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+import { animate, motion } from "framer-motion";
+
+const fadeInaAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
+
+const Skills = () => {
+  const { ref } = useSectionInView("Skills");
+  return (
+    <section
+      ref={ref}
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40 "
+    >
+      <SectionHeading text="My Skills" />
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            variants={fadeInaAnimationVariants}
+            className="bg-white border border-black/[0.1] rounded-xl px-5 py-3 "
+            key={index}
+            initial="initial"
+            whileInView={"animate"}
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
