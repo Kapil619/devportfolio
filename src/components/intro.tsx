8e41fbc3af0b07ef6611ddd54b7fe7126c92fab9
kapil619 <kapilbadokar321@gmail.com>
2023-11-10 18:50:57 +0530

    Added some missing classes

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -55,7 +55,7 @@ const Intro = () => {
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -66,7 +66,7 @@ const Intro = () => {
           Full-stack developer & Mobile App Developer.
         </span>
         I enjoy building webapps and mobile apps.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
@@ -77,7 +77,7 @@ const Intro = () => {
         <Link
           //group class for hover animation
           href="#contact"
-          className="group  bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105  transition"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105  transition"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
@@ -90,7 +90,7 @@ const Intro = () => {
           //group class for hover animation
           href="/CV.pdf"
           download
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full  outline-none focus:scale-110 hover:scale-110  active:scale-105  transition borderBlack dark:bg-white/10 "
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full  outline-none focus:scale-110 hover:scale-110  active:scale-105  transition cursor-pointer borderBlack dark:bg-white/10 "
         >
           Resume
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
@@ -98,7 +98,7 @@ const Intro = () => {
         <a
           href=" https://www.linkedin.com/in/kapil-badokar/"
           target="_blank"
-          className="bg-white p-4 text-gray-700  flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]  hover:text-gray-950 active:scale-105  transition borderBlack dark:bg-white/10 dark:text-white/60"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]  hover:text-gray-950 active:scale-105  transition borderBlack dark:bg-white/10 dark:text-white/60 cursor-pointer"
         >
           <BsLinkedin />
         </a>
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -29,10 +29,10 @@ export default function Project({
         scale: scaleProgress,
         opacity: opacityProgress,
       }}
-      className="group mb-3 sm:mb-8 last:mb-0 even:pl-8"
+      className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className=" bg-gray-100 max-w-[42rem] border-black/5 rounded-lg  overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20 dark:text-white ">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]   ">
+      <section className=" bg-gray-100 max-w-[42rem] border-black/5 rounded-lg  overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20 dark:text-white ">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]   ">
           <h3 className="text-2xl font-semibold "> {title} </h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 ">
             {description}
@@ -53,7 +53,7 @@ export default function Project({
           src={imageUrl}
           alt={title}
           quality={90}
-          className="absolute  top-8 -right-40  w-[28.25rem] rounded-t-lg shadow-2xl
+          className="absolute hidden sm:block top-8 -right-40  w-[28.25rem] rounded-t-lg shadow-2xl
         transition
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
