65388feb636a194ed2106055114e6ae25dc400b9
kapil619 <kapilbadokar321@gmail.com>
2023-11-10 12:17:09 +0530

    Completed Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Toaster position="top-right" />
+          <Footer />
         </ActiveSectionContextProvider>
       </body>
     </html>
---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block  text-xs ">
+        &copy; 2023 Kapil. All Rights reserved.
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
