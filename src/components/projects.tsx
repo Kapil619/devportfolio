d285a32716b1b98b1ccf90cf39746c7d8329ca2a
kapil619 <kapilbadokar321@gmail.com>
2023-11-13 22:27:41 +0530

    Finished projects section  with btn animations.

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,22 +4,27 @@ import { useSectionInView } from "@/lib/hooks";
 import React, { useState } from "react";
 import Project from "./project";
 import SectionHeading from "./sectionHeading";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
 
-  const initialProjectsToShow = 3; // Set the initial number of projects to show
+  //for show more btn
+  const initialProjectsToShow = 3;
   const [projectsToShow, setProjectsToShow] = useState(initialProjectsToShow);
-
   const handleShowMore = () => {
-    // Increase the number of projects to show when the "Show More" button is clicked
-    setProjectsToShow((prevCount) => prevCount + 3); // You can adjust the increment as needed
+    setProjectsToShow((prevCount) => prevCount + 3);
   };
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+    <section
+      ref={ref}
+      id="projects"
+      className="scroll-mt-28 mb-28 flex flex-col items-center"
+    >
       <SectionHeading text="My Projects" />
       <div>
+        {/* TO IMPLEMENT: FILTERING BY PLATFORM */}
         {/* <div className="flex flex-row justify-center items-center gap-2 py-4">
           <button className="rounded-full border-2 border-purple-500 px-6 py-3 cursor-pointer hover:border-white">
             All
@@ -41,12 +46,24 @@ export default function Projects() {
 
       {/* "Show More" button */}
       {projectsToShow < projectsData.length && (
-        <button
-          className="text-purple-600 cursor-pointer mt-4 inline-block"
+        <motion.button
+          initial={{
+            opacity: 0,
+          }}
+          whileInView={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 1,
+          }}
+          whileHover={{ scale: 1.1 }}
+          className=" cursor-pointer mt-4 inline-block bg-gray-900 text-white rounded-full outline-none transition-all  focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10
+          h-[3rem] w-[8rem]
+          "
           onClick={handleShowMore}
         >
           Show More
-        </button>
+        </motion.button>
       )}
     </section>
   );
